197550e9cddce9b3b71929bb3436d21a85ef9033
Timur_Umanets <umanexhttimur@gmail.com>
2025-04-16 11:04:03 +0300

    Update 111111.py

--- 111111.py
+++ 111111.py
@@ -1,16 +1,1 @@
-class Players:
-    def __init__(self):
-        # This dictionary will map the floor number to the number of players on that floor
-        self.players_on_floor = {}
-
-    def add_players(self, floor, num_players):
-        if floor in self.players_on_floor:
-            self.players_on_floor[floor] += num_players
-        else:
-            self.players_on_floor[floor] = num_players
-
-    def get_players_on_floor(self, floor):
-        return self.players_on_floor.get(floor, 0)
-
-    def get_total_players(self):
-        return sum(self.players_on_floor.values())
+aws
